b3ed054d41813dc71e66ff74894a828fb31f8da2
phr31 <phr31@hotmail.com>
2024-09-09 21:57:31 -0300

    vercel config cors

--- src/server.tsx
+++ src/server.tsx
@@ -1,23 +1,23 @@
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import { createTrip } from './routes/create-trip'
+import fastify from 'fastify';
+import cors from '@fastify/cors';
+import { createTrip } from './routes/create-trip';
 import {
   serializerCompiler,
   validatorCompiler,
-} from 'fastify-type-provider-zod'
-import { confirmTrip } from './routes/confirm-trip'
-import { confirmParticipants } from './routes/confirm-participant'
-import { createActivity } from './routes/create-activity'
-import { getActivities } from './routes/get-activities'
-import { createLink } from './routes/create-link'
-import { getLinks } from './routes/get-links'
-import { getParticipants } from './routes/get-participants'
-import { createInvite } from './routes/create-invite'
-import { updateTrip } from './routes/update-trip'
-import { getTripDetails } from './routes/get-trip-details'
-import { getParticipant } from './routes/get-participant'
-import { errorHandler } from './error-handler'
-import { env } from './env'
+} from 'fastify-type-provider-zod';
+import { confirmTrip } from './routes/confirm-trip';
+import { confirmParticipants } from './routes/confirm-participant';
+import { createActivity } from './routes/create-activity';
+import { getActivities } from './routes/get-activities';
+import { createLink } from './routes/create-link';
+import { getLinks } from './routes/get-links';
+import { getParticipants } from './routes/get-participants';
+import { createInvite } from './routes/create-invite';
+import { updateTrip } from './routes/update-trip';
+import { getTripDetails } from './routes/get-trip-details';
+import { getParticipant } from './routes/get-participant';
+import { errorHandler } from './error-handler';
+import { env } from './env';
 
 require('dotenv').config();
 
@@ -31,30 +31,35 @@ console.log('API Base URL:', apiBaseUrl);
 console.log('Web Base URL:', webBaseUrl);
 console.log('Port:', port);
 
-const app = fastify()
+const app = fastify();
 
 app.register(cors, {
-  origin: '*',
-})
+  origin: ['https://nlw-journey-node-js.vercel.app'], // Permite apenas o domínio do Vercel
+});
 
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.setErrorHandler(errorHandler)
+app.setErrorHandler(errorHandler);
 
-app.register(createTrip)
-app.register(confirmTrip)
-app.register(confirmParticipants)
-app.register(createActivity)
-app.register(getActivities)
-app.register(createLink)
-app.register(getLinks)
-app.register(getParticipants)
-app.register(createInvite)
-app.register(updateTrip)
-app.register(getTripDetails)
-app.register(getParticipant)
-
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipants);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
+app.register(updateTrip);
+app.register(getTripDetails);
+app.register(getParticipant);
+/*
 app.listen({ port: env.PORT }).then(() => {
   console.log('Server running!')
-})
+})*/
+app
+  .listen({ port: Number(process.env.PORT) || 3000, host: '0.0.0.0' })
+  .then(() => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+  });
